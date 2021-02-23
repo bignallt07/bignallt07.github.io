840eff2be5451a040ac1ee6edce63b028dca5608
Tom Bignall <bignallt@gmail.com>
2021-02-22 16:19:24 -0800

    add comments and update readme

--- README.md
+++ README.md
@@ -1,6 +1,41 @@
-# td6-Static-NodeJS-and-Express-Site
- Portfolio of my work, built with Node JS
+# td6-Static-NodeJS-and-Express-Site/Portfolio
+This app is a portfolio of my work as a developer, built with Node JS and Express. You can view all of my recent projects and access a live demo and github files. This app handles errors in a friendly way, as well as redirecting visitors in the right direction.
 
-# Changes to CSS
-* Error page styles
-* Background color
+## Motivation
+This project was built as a place to store all the work I would like to show others in a simple way.
+
+## Build Status
+The project template is built, however there will continously be projects added to this site
+
+## Code Style
+Node JS
+
+## Features
+* This page has a home, about and error pages
+* Project pages are created from Node JS routes and dynamically display depending on the page number
+
+## Notes for Treehouse Reviewer:
+* The image_url in package.json is a 2d array. I wanted to add a place to store the alt attribute that would go with the image
+* I have added an additional div at the bottom of index.pug. This holds a section for my upcoming projects 
+    * For the purpose of this project, no other HTML elements have been added excluding point above, and the error pages which extend the layout template
+* The url "localhost:3000/projects redirects to "localhost:3000/projects/0" so that an error page can be avoided
+
+* This project has been created to achieve extra credit. Here are some notes regarding that
+    1. package.json was edited to allow "npm start" to run the app. You will notice a link in the console added for user ease.
+    2. Error pages "error.pug" and "page-not-found.pug" were created and styled in extension of layout.pug
+    3. There are a number of CSS changes (mainly with the error pages) and in an effort follow DRY programming concept and to keep the media quieries at the foot of the stylesheet I have added changes to the following areas (note previous styles are commented out):
+        * Line 25: Background-color of the body changed
+        * Line 56: Main paragraph color changed
+        * Line 143: .sidebar color changed 
+        * Line 174: .sidebar paragraph color changed
+        * Line 269-274: Additional styles added for the additional homepage div
+        * Line 293: .about-bio paragraph padding-top removed
+        * Line 322: .btn-link box shadow added
+        * Line 325-389: Error Page Styles Added
+
+## Contribution
+If you have any suggestions to improve this app, please reach out to me on github
+
+## Credits
+Curriculum at teamtreehouse.com
+
--- app.js
+++ app.js
@@ -1,52 +1,96 @@
-// Require necessary files and modules
+/*************************************
+ * Basic Node and Express Set up
+ *      1. Require Express
+ *      2. Require Data from data.json
+ *      3. Create Express AppRequiring Files and Modules 
+ ************************************/
 const express = require("express");
-
 const data = require("./data.json");
 
-// Start app
 const app = express();
 
-// Set up middleware
+
+/*************************************
+ * Middlewear Set up
+ *      1. Setting view engine to pug
+ *      2. Set up static files
+ ************************************/
+
 app.set('view engine', 'pug');
 app.use('/static', express.static('public'));
 
-// routes
-// home route
+
+/*************************************
+ * Declaring Routes
+*************************************/ 
+
+/**
+ * 1. Home Route
+ * 
+ * Stores data to route locals and renders index page
+ */
 app.get("/", (req, res) => {
     res.locals = data.projects;
     res.render("index", {projects: res.locals});
 });
 
+/**
+ * About Route
+ */
 app.get("/about", (req, res) => {
     res.render("about");
 });
 
+/**
+ * project redirect route
+ * 
+ * This redirects user if they don't add an id parmeter
+ */
 app.get('/projects', (req, res) => {
     res.redirect("/projects/0");
 });
 
+/**
+ * Projects Route
+ * 
+ * 1. Listens for a ID parameter and renders the correct project page
+ * 2. This also reacts to incorrect id's and sends to error middlewear
+ */
 app.get("/projects/:id", (req, res, next) => {
     const id = req.params.id;
     if (id < data.projects.length) {
         const project = data.projects[id];
         res.render("project", project);
     }  else {
-        next(); // Include middlewear to skip to another page
+        next(); // redirect to error middlewear
     }
 });
 
-// Error Routes
-// Creating 404 error
+/*************************************
+ * Error Handling
+*************************************/ 
+
+/**
+ * Middlewear to create 404 error.
+ *        1. Creates a new error, with message. 
+ *        2. Sets the status, and sends to the global error handler
+ */
 app.use((req, res, next) => {
-    const err = new Error("There has been an error loading the page");            // Create error and message
-    err.status = 404;                                   // Defines the 404 = not found
-    next(err);                                          // Sends the error to the next middlewear
+    const err = new Error("There has been an error loading the page");           
+    err.status = 404;                                  
+    next(err);                                         
 });
 
-// Global Error
+/**
+ * Global Error Handler
+ * 
+ *        1. Listens for error status type and stores it in the response locals 
+ *        2. Checks the status of the error and renders the correct error page. With Error objects parameters
+ *        3. Logs error to the console     
+ */
 app.use((err, req, res, next) => {
-    res.locals.error = err;                             // Set locals to error, whether it be 404 or global
-    res.status(err.status);                             // Whatever the response status code is, is set  
+    res.locals.error = err;                            
+    res.status(err.status);                           
     console.error(`Error Occured... Status: ${err.status} - ${err.message}`);
     if (err.status === 404) {                         
         res.render("page-not-found", err);    
@@ -55,6 +99,9 @@ app.use((err, req, res, next) => {
     }
 });
 
+/*************************************
+ * Set up server on port: 3000 with console message
+*************************************/ 
 
 app.listen(3000, () => {
     console.log(`The app is listening on port 3000 - Follow Link: http://localhost:3000/`);
